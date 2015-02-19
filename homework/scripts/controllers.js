8ca9c16fecc02af6245f9dd490fec1c7b3058aab
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-02-19 09:08:04 -0600

    Added submission success ng-hide validator. Successmessage now shows on valide form submission.

--- homework/scripts/controllers.js
+++ homework/scripts/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
 .controller('SignUpCtrl', function($scope, $location){
-
+$scope.submissionSuccess = false;
 	$scope.submit = function() {
 		if($scope.userForm.$valid) {
 			console.log('valid sumbission');
