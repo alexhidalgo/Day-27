430485fc82477aa47f38a52046e53ad8d12a3970
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-02-19 10:02:05 -0600

    Rolled back changes to original

--- homework/scripts/controllers.js
+++ homework/scripts/controllers.js
@@ -1,10 +1,12 @@
 angular.module('app.controllers', [])
 .controller('SignUpCtrl', function($scope, $location){
 $scope.submissionSuccess = false;
+$scope.submissionMessage = false;
 	$scope.submit = function() {
 		if($scope.userForm.$valid) {
 			console.log('valid sumbission');
 			$scope.submissionSuccess = true;
+			$scope.submissionMessage = true;
 		}
 	};
 });
